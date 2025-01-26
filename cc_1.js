e41e09cf39fabe5a289cb620e9ee2dfa1616ac5c
kani <kanishkas@usf.edu>
2025-01-26 12:19:52 -0500

    Task 2 - Product Details

--- cc_1.js
+++ cc_1.js
@@ -5,3 +5,12 @@ console.log("Employee Name: " + employeeName, "Type: " + typeof employeeName);
 console.log("Employee ID: " + employeeID, "Type: " + typeof employeeID);
 console.log("Is Active: " + isActive, "Type: " + typeof isActive);
 
+// Task 2 - Product Details
+
+let productName = "Wireless Mouse";  
+const productPrice = 29.99;          
+var isAvailable = true;              
+
+console.log("Product Name: " + productName + ", Type: " + typeof productName);
+console.log("Product Price: " + productPrice + ", Type: " + typeof productPrice);
+console.log("Is Available: " + isAvailable + ", Type: " + typeof isAvailable);
