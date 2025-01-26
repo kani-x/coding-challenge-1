a3a5f1ae3c3e9a978ca747c6d798ba67aff08426
kani <kanishkas@usf.edu>
2025-01-26 12:09:27 -0500

    Task 1 - Employee Information

---
+++ cc_1.js
@@ -0,0 +1,7 @@
+let employeeName = "Harry Potter";
+const employeeID = 12345;
+var isActive = true;
+console.log("Employee Name: " + employeeName, "Type: " + typeof employeeName);
+console.log("Employee ID: " + employeeID, "Type: " + typeof employeeID);
+console.log("Is Active: " + isActive, "Type: " + typeof isActive);
+
